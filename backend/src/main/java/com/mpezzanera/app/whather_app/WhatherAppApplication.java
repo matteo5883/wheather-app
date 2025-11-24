1741ac13611aca190bdecb5ebf252a0813014941
matteo5883 <matteo.pezzanera@gmail.com>
2025-11-24 09:57:20 +0100

    initialize backend java project
    Using:
    - Java 21
    - SpringBoot 3.5.9 Snapshot
    - gradle

---
+++ backend/src/main/java/com/mpezzanera/app/whather_app/WhatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.mpezzanera.app.whather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatherAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mpezzanera/app/whather_app/WhatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpezzanera.app.whather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
